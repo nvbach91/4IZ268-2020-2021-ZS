d4ad3c24f7ef16fc38842668d412a7d7fad13dab
apatenko369 <72221824+apatenko369@users.noreply.github.com>
2020-11-25 17:18:28 +0100

    apai00-hw09

---
+++ www/apai00/hw09/assets/js/main.js
@@ -0,0 +1,82 @@
+/**
+ * Long live Sparta! Vytvořte funkci, která vyřeší Caesarovu širfu. Funkce dostane 
+ * na vstup zašifrovaný text a také hodnotu, která byla použita při šifrování, a pak 
+ * vrátí dešifrovaný text. Předpokládejte pouze anglickou abecedu s velkými 
+ * písmeny, ostatní znaky ignorujte. Poté v konzoli dešifrujte/dešiftujte následující texty.
+ * 
+ * key used - encrypted text
+ *       19 - MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG
+ *        5 - YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW
+ *       12 - M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ
+ * 
+ * Následně vytvořte uživatelské rozhraní, ve kterém bude možné zadat zmíněné dvě 
+ * vstupní hodnoty (zašifrovaný text a použitý klíč) a po kliknutí na tlačítko 
+ * "Decipher!" se na určeném místě zobrazí dešifrovaný text. Rozhraní také vhodně
+ * nastylujte.
+ */
+//              0123456789...
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const shiftChar = (c, shift) => {
+    // a helper function to shift one character inside the 
+    // alphabet based on the shift value and return the result
+    let charcode = c.toUpperCase().charCodeAt() - shift;
+    /*ASCI CODE*/
+    if (charcode < 65) {
+        charcode = charcode + 26;
+    }
+    /*return and convert unicode ASCI*/
+    return String.fromCharCode(charcode);
+};
+const shiftString = (str, shift) => {
+    // a helper function to shift one entire string inside the 
+    // alphabet based on the shift value and return the result
+    let output = '';
+    for (i = 0; i < str.length; i++) {
+        if ([':', '.', ',', '-', ' '].includes(str[i])) {
+            output = output + ' ';
+            continue;
+        }
+        output = output + shiftChar(str[i], shift);
+    }
+    return output;
+};
+const caesarDecipher = (cipherText, usedKey) => {
+    // your implementation goes here
+    // good to know: 
+    //    str.indexOf(c) - returns the index of the specified character in the string
+    //    str.charAt(i) - returns the character at the specified index in the string
+    //    when the shifted character is out of bound, it goes back to the beginning and count on from there
+
+    return document.innerText = shiftString(cipherText, usedKey);
+};
+
+
+// albert einstein
+caesarDecipher("MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG", 19);
+
+// john archibald wheeler
+caesarDecipher("YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW", 5);
+
+// charles darwin
+caesarDecipher("M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ", 12);
+
+
+
+const button = document.querySelector('#button');
+const key = document.querySelector('#key');
+const input = document.querySelector('#input-text')
+const answer = document.querySelector('#answer');
+
+button.addEventListener('click', () => {
+    const usedKey = key.value;
+    const cipherText = input.value;
+
+    if (key.value < 26) {
+        const duo = caesarDecipher(cipherText, usedKey);
+        const deciphered = duo + '<br>' + '<br>';
+        answer.innerHTML = answer.innerHTML + deciphered;
+    } else {
+        alert("Zadejte číslo které není větší než 25!");
+    }
+});
