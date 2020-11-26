329fa73e57a059545b2ac2a73eae7bfa1919600b
madkhab <72251466+madkhab@users.noreply.github.com>
2020-11-26 23:10:12 +0100

    homework n.9
    https://eso.vse.cz/~kham06/cv09/homework

---
+++ www/cv09/homework/main.js
@@ -0,0 +1,88 @@
+/**
+ * Long live Sparta! Vytvořte funkci, která vyřeší Caesarovu širfu. Funkce dostane 
+ * na vstup zašifrovaný text a také hodnotu, která byla použita při šifrování, a pak 
+ * vrátí dešifrovaný text. Předpokládejte pouze anglickou abecedu s velkými 
+ * písmeny, ostatní znaky ignorujte. Poté v konzoli dešifrujte/dešiftujte následující texty.
+ * 
+ * key used - encrypted text
+ *       19 - MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG
+ *        5 - YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW
+ *       12 - M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ
+ * 
+ * Následně vytvořte uživatelské rozhraní, ve kterém bude možné zadat zmíněné dvě 
+ * vstupní hodnoty (zašifrovaný text a použitý klíč) a po kliknutí na tlačítko 
+ * "Decipher!" se na určeném místě zobrazí dešifrovaný text. Rozhraní také vhodně
+ * nastylujte.
+ */
+//              0123456789...
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const shiftChar = (c, shift) => {
+    // a helper function to shift one character inside the 
+    // alphabet based on the shift value and return the result
+    let i = alphabet.indexOf(c);
+    if (i - shift < 0) {
+        i = i + 26 ; 
+    }
+    c = alphabet.charAt(i - shift);
+    return c;
+};
+
+function IsInArray(value, array){
+    return array.indexOf(value) > -1;
+};
+
+const shiftString = (str, shift) => {
+    // a helper function to shift one entire string inside the 
+    // alphabet based on the shift value and return the result
+    let i;
+    shiftedString = '';
+    for ( i = 0; i < str.length; i++){
+        if (IsInArray (str.charAt(i), alphabet.split(''))){
+            shiftedChar = shiftChar(str.charAt(i), shift);
+        }
+        else{
+            shiftedChar = str.charAt(i);
+        };
+        shiftedString = shiftedString + shiftedChar;
+    } 
+    return shiftedString;
+};
+
+const caesarDecipher = (cipherText, keyNumber) => {
+    // your implementation goes here
+    // good to know: 
+    //    str.indexOf(c) - returns the index of the specified character in the string
+    //    str.charAt(i) - returns the character at the specified index in the string
+    //    when the shifted character is out of bound, it goes back to the beginning and count on from there
+    deciphered = shiftString(cipherText.toUpperCase(), keyNumber);
+    return deciphered;
+};
+const CipherTextPlace = document.querySelector('#inputcipher');
+const InputKeyPlace = document.querySelector('#inputkey');
+const DecipherButton = document.querySelector('#buttondecipher');
+const OutputPlace = document.querySelector('#output');
+const Table = document.querySelector('#table');
+
+DecipherButton.addEventListener('click', (e) => {
+    let cipherText = CipherTextPlace.value;
+    let usedKey = InputKeyPlace.value;
+    if( InputKeyPlace.value > 26) {
+        Table.insertBefore(impossible, DecipherButton);
+    } 
+    else {
+        OutputPlace.innerText = caesarDecipher(cipherText, usedKey);
+    }
+    e.preventDefault();
+});
+
+// albert einstein
+caesarDecipher("MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG", 19);
+console.log(caesarDecipher("MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG", 19));
+// john archibald wheeler
+caesarDecipher("YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW", 5);
+console.log(caesarDecipher("YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW",5));
+
+// charles darwin
+caesarDecipher("M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ", 12);
+console.log(caesarDecipher("M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ",12));
