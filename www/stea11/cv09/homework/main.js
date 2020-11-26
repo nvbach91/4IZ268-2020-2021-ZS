0f746ec1d956ae953c74caa702ad669b1346f530
anetapls <72199532+anetapls@users.noreply.github.com>
2020-11-26 20:41:30 +0100

    stea11-hw09

---
+++ www/stea11/cv09/homework/main.js
@@ -0,0 +1,68 @@
+/**
+ * Long live Sparta! Vytvořte funkci, která vyřeší Caesarovu širfu. Funkce dostane 
+ * na vstup zašifrovaný text a také hodnotu, která byla použita při šifrování, a pak 
+ * vrátí dešifrovaný text. Předpokládejte pouze anglickou abecedu s velkými 
+ * písmeny, ostatní znaky ignorujte. Poté v konzoli dešifrujte/dešiftujte následující texty.
+ * 
+ * key used - encrypted text
+ *       19 - MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG
+ *        5 - YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW
+ *       12 - M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ
+ * 
+ * Následně vytvořte uživatelské rozhraní, ve kterém bude možné zadat zmíněné dvě 
+ * vstupní hodnoty (zašifrovaný text a použitý klíč) a po kliknutí na tlačítko 
+ * "Decipher!" se na určeném místě zobrazí dešifrovaný text. Rozhraní také vhodně
+ * nastylujte.
+ */
+//              0123456789...
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const shiftChar = (c, shift) => {
+    // a helper function to shift one character inside the 
+    // alphabet based on the shift value and return the result
+    let position = alphabet.indexOf(c);
+    // když idexOf vrati -1, znamena to ze znak 'c' neni v abecede 'alphabet'
+    if (position == -1) {
+        return c;
+    }
+    let newPosition = (position + shift) % alphabet.length;
+    return alphabet.charAt(newPosition);
+};
+const shiftString = (str, shift) => {
+    // a helper function to shift one entire string inside the 
+    // alphabet based on the shift value and return the result
+    let shiftedStr = "";
+    for (let c of str) {
+        shiftedStr = shiftedStr + shiftChar(c, shift);
+    }
+    return shiftedStr;
+};
+const caesarDecipher = (cipherText, usedKey) => {
+    // your implementation goes here
+    // good to know: 
+    //    str.indexOf(c) - returns the index of the specified character in the string
+    //    str.charAt(i) - returns the character at the specified index in the string
+    //    when the shifted character is out of bound, it goes back to the beginning and count on from there
+    const decipheredText = shiftString(cipherText, alphabet.length - usedKey);
+
+    return decipheredText;
+};
+
+// albert einstein
+caesarDecipher("MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG", 19);
+
+// john archibald wheeler
+caesarDecipher("YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW", 5);
+
+// charles darwin
+caesarDecipher("M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ", 12);
+
+function decipher() {
+    let textElement = document.getElementById("text")
+    let shiftElement = document.getElementById("shift")
+    let resultElement = document.getElementById("result")
+
+    let result = caesarDecipher(textElement.value, shiftElement.value)
+
+    resultElement.textContent = result
+}
