4afe8f2700f8805ca861225863e0954fc5e52c24
Bejt02 <72217697+Bejt02@users.noreply.github.com>
2020-12-20 00:58:31 +0100

    Add files via upload

--- www/bejt02/sp2/js/main.js
+++ www/bejt02/sp2/js/main.js
@@ -1,17 +1,19 @@
-const api = {
-    baseurl: "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a",
-};
-
 $(document).ready(function(){
     $("button").click(function(){
-        $("#answer").empty();
+        $("#obrazky").empty();
         var letter = $(this).text();
         $.ajax({url: "https://thecocktaildb.com/api/json/v1/1/search.php?f="+letter, success: function(result){
         var myJSON = JSON.stringify(result);
-        $("#answer").html(myJSON);
+        console.log(result.drinks.length);
+        var i;
+        for (i = 0; i < result.drinks.length; i++) {
+            $("#obrazky").append("<p id='nazev"+i+"'>"+result.drinks[i].strDrink+"</img>");
+            $("#obrazky").append("<img id='obrazek"+i+"' src='"+result.drinks[i].strDrinkThumb+"'></img>");
+        }
         }});
     });
 });
+
 
 /*
 Zatím není hotový styling pro vypsané hodnoty, bude dopracováno pro finální verzi.
