0f4bd0e5b2761dc630607948dc959a327ca04801
michalmrkus <72578739+michalmrkus@users.noreply.github.com>
2020-12-20 19:42:32 +0100

    mrkm00 - sp2

---
+++ www/mrkm00/sp2/assets/js/main.js
@@ -0,0 +1,3 @@
+const postForm = document.querySelector ('#post-search-form');
+const hashtagInput = ocument.querySelector ('#post-search-form [name="hashtag-input"]');
+const postsList = document.querySelector ('#posts-list');
