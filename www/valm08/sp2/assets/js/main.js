7d85a8102e4f6b0c8473bf78c388b5a7ff19715b
valm08 <72195509+valm08@users.noreply.github.com>
2021-01-06 23:15:41 +0100

    version 02
    Většina funkcionality.
    Zdá se, že PDF soubory nelze stahovat přes AJAX kvůli CORS (ale citace ano?), takže jsou prostě stahovány přímo z endpointu a nejsou zazipované.
    
    https://eso.vse.cz/~valm08/sp2/

--- www/valm08/sp2/assets/js/main.js
+++ www/valm08/sp2/assets/js/main.js
@@ -4,6 +4,23 @@ $(document).ready(() => {
   const desktopPdfButton = $("#desktop-pdf");
   const mobilePdfButton = $("#mobile-pdf");
   const inputField = $("#input-field");
+  const zoteroButton = $("#zotero");
+  const bibtexButton = $("#bibtex");
+  const outputField = $("#output-field");
+  const favouriteButton = $("#favourite");
+  const clearFavouritesButton = $("#clear-favourites");
+  const showFavouritesButton = $("#show-favourites");
+  //const removeFavouriteButton = $("#remove-favourite");
+
+  //based on https://javascript.info/localstorage
+  function renderFavourites() {
+    for(let i=0; i<localStorage.length; i++) {
+    let key = localStorage.key(i);
+    $(outputField).append(`${localStorage.getItem(key)} <br>`);
+    };
+  };
+
+  renderFavourites();
 
 
   function spaceToUnderscore (text) {
@@ -11,24 +28,115 @@ $(document).ready(() => {
     return text;
   }
 
+  
   //from https://stackoverflow.com/questions/1066452/easiest-way-to-open-a-download-window-without-navigating-away-from-the-page
   function downloadFile(filePath){
-    var link=document.createElement('a');
+    let link=document.createElement('a');
     link.href = filePath;
     link.download = filePath.substr(filePath.lastIndexOf('/') + 1);
     link.click();
-}
+  }
+
 
   $(mobilePdfButton).click(function(){
     let input = inputField.val();
+    input = input.trim();
     input = spaceToUnderscore(input);
     downloadFile(`https://en.wikipedia.org/api/rest_v1/page/pdf/${input}/a4/mobile`);
   });
 
+
   $(desktopPdfButton).click(function(){
     let input = inputField.val();
+    input = input.trim();
     input = spaceToUnderscore(input);
     downloadFile(`https://en.wikipedia.org/api/rest_v1/page/pdf/${input}/a4`);
   });
+
+
+  $(zoteroButton).click(function(){
+    let input = inputField.val();
+    input = input.trim();
+    input = encodeURIComponent(input);
+    $.ajax({
+      method: 'GET',
+      dataType: "json",
+      url: `https://en.wikipedia.org/api/rest_v1/data/citation/zotero/${input}`,
+      success: (resp) => {
+        text = JSON.stringify(resp);
+        $(outputField).text(text);
+      },
+      error: (error) => {$(outputField).text("Error. Please check the URL.");},
+    });
+  });
+
+
+  $(bibtexButton).click(function(){
+    let input = inputField.val();
+    input = input.trim();
+    input = encodeURIComponent(input);
+    $.ajax({
+      method: 'GET',
+      url: `https://en.wikipedia.org/api/rest_v1/data/citation/bibtex/${input}`,
+      success: (resp) => {
+        text = resp;
+        $(outputField).text(text);
+      },
+      error: (error) => {$(outputField).text("Error. Please check the URL.");},
+    });
+  });
+
+
+  $(favouriteButton).click(function(){
+    for(let i=0; i<localStorage.length; i++) {
+      let key = localStorage.key(i);
+      if (localStorage.getItem(key) === inputField.val()) {
+        alert("This already is in your list of favourites.");
+        return;
+      };
+      };
+    localStorage.setItem(localStorage.length,inputField.val());
+    $(outputField).text("");
+    renderFavourites();
+  });
+
+
+  $(clearFavouritesButton).click(function(){
+    localStorage.clear();
+    $(outputField).text("");
+    renderFavourites();
+  });
+
+  $(showFavouritesButton).click(function(){
+    $(outputField).text("");
+    renderFavourites();
+  });
+
+  /*
+  $(removeFavouriteButton).click(function(){
+    for(let i=0; i<localStorage.length; i++) {
+      let key = localStorage.key(i);
+      if (localStorage.getItem(key) === inputField.val()) {
+        localStorage.removeItem(key);
+        $(outputField).text("");
+        renderFavourites();
+        return;
+      };
+    };
+    alert("This is not in your list of favourites.");
+    $(outputField).text("");
+    renderFavourites();
+  });*/
+
+
+  //based on https://stackoverflow.com/questions/1964839/how-can-i-create-a-please-wait-loading-animation-using-jquery
+  $(document).on({
+    ajaxStart: function(){
+      $(outputField).text("");
+      $(outputField).text("Please wait. This may take up to 10 seconds.");
+    }
+  });
+
+  
 });
 
