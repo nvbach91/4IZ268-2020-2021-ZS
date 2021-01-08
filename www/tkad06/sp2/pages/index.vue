cf20c93698a2ec3383e59e54bb10ae701d2ded16
Dmitrij Tkacenko <dimot9@gmail.com>
2021-01-08 10:36:49 +0100

    :sparkles: Auroredirect from login page to app when logged in

--- www/tkad06/sp2/pages/index.vue
+++ www/tkad06/sp2/pages/index.vue
@@ -6,5 +6,12 @@
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    // make on client side, when accessing from server, because auth module has always loggedIn = false on server side
+    if (this.$auth.loggedIn) {
+      this.$router.push({ name: 'shelf' })
+    }
+  },
+}
 </script>
--- www/tkad06/sp2/plugins/notifications.js
+++ www/tkad06/sp2/plugins/notifications.js
@@ -1,12 +1,5 @@
-import * as notificationsStore from '~/store/notifications'
 import notificationsApi from '~/lib/notificationsApi'
 
-export default ({ isClient, store }, inject) => {
-  const opts = {}
-  if (isClient) {
-    opts.preserveState = true
-  }
-  store.registerModule('notifications', notificationsStore, opts)
-
+export default ({ store }, inject) => {
   inject('notify', notificationsApi(store))
 }
