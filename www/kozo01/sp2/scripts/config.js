746be29687d5e1215e8b68b8c179c53d49ab30d6
oli711 <72568430+oli711@users.noreply.github.com>
2021-01-22 16:39:41 +0100

    Verze 2
    Opraveno všechno kromě #6

--- www/kozo01/sp2/scripts/config.js
+++ www/kozo01/sp2/scripts/config.js
@@ -1,5 +1,9 @@
-const API_KEY = '1b4d4d105eaf653e9e43a4f3a0300fc1';
-
+const API_KEY = '1b4d4d105eaf653e9e43a4f3a0300fc1'; // Api Key OpenWeathermap
+// Тосты Сообщения/Уведомления с ошибками
+/*
+1 - Ошибка неправильного ввода кода
+2 - Макс
+*/
 const TOAST_MSG = {
     INVALID_SEARCH: 'Please enter a valid city or US state.',
     MAX_REQUESTS: 'We have reached the maximum number of requests for weather data.'
