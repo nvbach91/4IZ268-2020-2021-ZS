96da6339dce6223bc3118ff3911777310854e998
andreaborekova <72227310+andreaborekova@users.noreply.github.com>
2021-01-23 00:23:41 +0100

    bora13-sp2
    pouzijte localStorage pro ukladani oblibenych filmu=> funguje mi ulozenie filmu pomocou tlacidla SAVE, ale nemam dokonceny arraylist, a tym padom sa ulozeny film stale prepisuje
    pridejte treti view pro zobrazeni oblibenych filmu + moznost jejich vymazu=> view sa zobrazi pomocou tlacidla MY FAVORITES (vymaz zatial chyba koli nefunkcnemu arraylistu)
    aplikace nesmi uzivatele nikam presmerovavat=> X
    vysledky z vyhledavani nemazte, uzivatel bude chtit se vratit k puvodnimu seznamu vysledku vyhledavani=>  X
    opravte rozmery obrazku, a pouzivejte lepsi rozliseni obrazku => upraveny rozmer obrazkov

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function getMovies(searchText) {
                 <div class="well text-center">
                     <img src="${movie.Poster}">
                     <h5>${movie.Title}</h5>
-                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#" >Movie Details</a>
                 </div>
             </div>
             `;
@@ -35,12 +35,26 @@ function getMovies(searchText) {
 }
 function movieSelected(id) {
     sessionStorage.setItem('movieId', id);
+    localStorage.setItem('movieId', id);
+    console.log("sme v movieSelected");
+    console.log(localStorage.getItem('movieId'));
     window.location = 'movie.html';
     return false;
 }
+function movieSaved(filmId) {
+    localStorage.getItem('movieId', filmId);
+    let idf = filmId;
+    console.log("sme v movieSaved");
+    console.log(idf);
+    console.log(JSON.stringify(idf));
+    localStorage.setItem('favoriteMovies', JSON.stringify(idf));
+}
 
 function getMovie() {
-    let movieId = sessionStorage.getItem('movieId');
+  let movieId = sessionStorage.getItem('movieId');
+  localStorage.setItem('movieId', movieId);
+  console.log("sme v getMovie");
+  console.log(movieId);
 
     var url = "http://www.omdbapi.com/?apikey=" + apikey
 
@@ -73,6 +87,9 @@ function getMovie() {
              <div class="well">
               <a href="index.html" class="btn btn-default">Go back</a>
              </div>
+             <div>
+              <button onclick="movieSaved('${movie.imdbID}')" class="btn btn-primary" id="save-button">Save me</button>
+             </div>
         </div>
         `;
             $('#movie').html(output);
@@ -83,3 +100,53 @@ function getMovie() {
             console.log(err);
     });
 }
+function getFavMovie() {
+    var retrievedData = localStorage.getItem('favoriteMovies');
+   
+    console.log("sme v getFavMovie");
+    console.log(retrievedData);
+
+    var movies2 = JSON.parse(retrievedData);
+
+    var url = "http://www.omdbapi.com/?apikey=" + apikey
+
+    $.ajax({
+        method: 'GET',
+        url: url + "&i=" + movies2,
+        success: function (data) {
+            console.log(data);
+            let movie = data;
+
+            let output = `
+        <div class="row">
+             <div class="col-md-8">
+                <h2>${movie.Title}</h2>
+                </ul>
+             </div>
+        </div>
+        <div class="row">
+             <div>
+              <button onclick="movieSaved()" class="btn btn-primary" id="save-button">Save me</button>
+             </div>
+        </div>
+        
+ 
+        `;
+            $('#movie-list').html(output);
+            console.log("konieeec");
+        }
+    })
+    .catch((err) => {
+            console.log(err);
+    });
+}
+function myFunction() {
+    console.log("sme v myfunction");
+    var x = document.getElementById("favorites");
+    getFavMovie();
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
